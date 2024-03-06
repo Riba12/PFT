957d01e27796e47ef64384ffc2643d139611ecf9
Riba12 <110785802+Riba12@users.noreply.github.com>
2024-03-06 17:56:47 +0000

    duas cartas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   const [cartas, setCartas] = useState(["2", "3", "4", "5", "6", "7", "9", "T", "J", "K", "Q", "A"]);
@@ -9,9 +9,18 @@ function App() {
 
     let naipe1 = Math.floor(Math.random() * 4);
     let carta1 = Math.floor(Math.random() * 12);
-    let hand = [cartas[carta1],naipes[naipe1]];
+    let naipe2 = Math.floor(Math.random() * 4);
+    let carta2 = Math.floor(Math.random() * 12);
 
-    console.log(naipe1, carta1)
+    while(carta1 == carta2 && naipe1 == naipe2){
+      carta2 = Math.floor(Math.random() * 12);
+      naipe2 = Math.floor(Math.random() * 4);
+    }
+
+    let hand = [cartas[carta1], naipes[naipe1],cartas[carta2],naipes[naipe2]];
+
+    
+    console.log(hand)
     return hand
   }
 
